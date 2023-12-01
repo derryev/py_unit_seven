4f3060465d4545fb469aaa8044529b19f0ffc38c
derryev <derryev@ssfs.org>
2023-12-01 13:59:03 -0500

    completed upper_lower

--- upper_lower.py
+++ upper_lower.py
@@ -1,7 +1,16 @@
 
 
+def upper_lower(word):
+    lowercase = word.lower()
+    uppercase = word.upper()
+    processed_output = lowercase + " " + uppercase
+    print(processed_output)
+
+
 def main():
-    pass
+    user_input = input("Please write what you would like to have converted to upper and lower case:")
+    upper_lower(user_input)
+
 
 if __name__ == '__main__':
     main()
