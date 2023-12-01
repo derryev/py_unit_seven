87f4e4affb98fe0183a0d01c082a07d4032723d7
derryev <derryev@ssfs.org>
2023-12-01 14:00:34 -0500

    completed upper_lower and printing letters

--- upper_lower.py
+++ upper_lower.py
@@ -7,9 +7,15 @@ def upper_lower(word):
     print(processed_output)
 
 
+def printing_letters(word):
+    for character in word:
+        print(character)
+
+
 def main():
     user_input = input("Please write what you would like to have converted to upper and lower case:")
     upper_lower(user_input)
+    printing_letters(user_input)
 
 
 if __name__ == '__main__':
