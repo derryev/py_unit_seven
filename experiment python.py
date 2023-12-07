5628d645c5e111dc887a8540ec362b0e2248ff11
derryev <derryev@ssfs.org>
2023-12-07 11:59:39 -0500

    Working on strings project.

--- experiment python.py
+++ experiment python.py
@@ -4,20 +4,60 @@ import tkinter as tk
 
 root = tk.Tk()
 root.title("Climbing Score Calculator")
-root.configure(background = "white")                                                                                    #figured out how to make ceritan background color from this website: https://stackoverflow.com/questions/2744795/background-color-for-tk-in-python
-introduction = tk.Label(text = "Welcome to the bouldering competition points cointer!", fg="purple", bg="white")
+
+boulders_list = ["Vintro", "V0", "V1", "V2", "V3", "V4", "V5", "V6", "V7", "V8", "V9", "V10"]
+selected_boulder1 = tk.StringVar()
+selected_boulder2 = tk.StringVar()
+selected_boulder3 = tk.StringVar()
+
+introduction = tk.Label(text = "Welcome to the bouldering competition points counter!", fg="magenta")
+
+def determine_dropdowns():
+    number_of_menus = int(climb_input.get())
+    menu_instructions = tk.Label(text="Select the grades of the highest-graded climbs you completed:",fg="magenta")
+    menu_instructions.grid(row=6,column=2,columnspan=1)
+    if number_of_menus == 2:
+        ## I learned how to make a dropdown menu from https://www.tutorialsfreak.com/python-tutorial/drop-down-menus-in-python-tkinter, but I did the grid myself.
+        dropdown1 = tk.OptionMenu(root, selected_boulder1, "Vintro", "V0", "V1", "V2", "V3", "V4", "V5",
+                                  "V6","V7", "V8", "V9", "V10")
+        dropdown2 = tk.OptionMenu(root, selected_boulder2, "Vintro", "V0", "V1", "V2", "V3", "V4", "V5",
+                                  "V6", "V7", "V8", "V9", "V10")
+        dropdown1.grid(row=7, column=2, columnspan=2)
+        dropdown2.grid(row=7, column=3, columnspan=2)
+    elif number_of_menus == 3:
+        dropdown1 = tk.OptionMenu(root, selected_boulder1, "Vintro", "V0", "V1", "V2", "V3", "V4", "V5",
+                                  "V6", "V7", "V8", "V9", "V10")
+        dropdown2 = tk.OptionMenu(root, selected_boulder2, "Vintro", "V0", "V1", "V2", "V3", "V4", "V5",
+                                  "V6", "V7", "V8", "V9", "V10")
+        dropdown3 = tk.OptionMenu(root, selected_boulder3, "Vintro", "V0", "V1", "V2", "V3", "V4", "V5",
+                                  "V6", "V7", "V8", "V9", "V10")
+        dropdown1.grid(row=7, column=2, columnspan=3)
+        dropdown2.grid(row=7, column=3, columnspan=3)
+        dropdown3.grid(row=7, column=4, columnspan=3)
 
 
-climb_input = tk.Label(text = "How many climbs are you doing for points?", fg="purple")
+
+climb_input_question = tk.Label(text = "Are you entering 2 or 3 climbs?", fg="magenta")
+climb_input = tk.StringVar()
+climb_input.set("2")
+climb_input_entry = tk.Entry(root, textvariable=climb_input, fg = "cyan")
+button_for_number_of_dropdowns = tk.Button(root, text="Enter", command=determine_dropdowns,fg="magenta")
+#print(climb_input)
 
 number = tk.IntVar()
 number.set(0)
 #if climb_input = "Yes"
 
 
+# dropdown = tk.OptionMenu(root, selected_boulder, "Vintro", "V0", "V1", "V2", "V3", "V4", "V5", "V6",      #I learned how to make a dropdown menu from https://www.tutorialsfreak.com/python-tutorial/drop-down-menus-in-python-tkinter, but I did the grid myself.
+                         #"V7", "V8", "V9", "V10")
+
 
 introduction.grid(row=1, column=2, columnspan = 5)
-climb_input.grid(row=3, column = 2, columnspan = 8)
+climb_input_question.grid(row=3, column = 2, columnspan = 8)
+climb_input_entry.grid(row=4, column = 2, columnspan=8)
+button_for_number_of_dropdowns.grid(row=5,column=2,columnspan=8)
+# dropdown.grid(row=7, column=2,columnspan=8)
 
 
 
