1855a18db9867ff954aa2df14e3eac408aa7ed4c
derryev <derryev@ssfs.org>
2023-12-12 22:54:44 -0500

    In addition to completed bouldering and top rope competition format calculators, completed championship competition format point calculator.

--- ActualUnit7Project.py
+++ ActualUnit7Project.py
@@ -32,6 +32,18 @@ def determine_dropdowns():
         dropdown3.grid(row=7, column=3, columnspan=3)
         calculate_score_button = tk.Button(root, text="Calculate Score", command=calculate_score_boulders, fg="magenta")
         calculate_score_button.grid(row=8, column=2, columnspan=3)
+    elif number_of_menus == "Champs":
+        climb_label_b = tk.Label(text="Boulders:", fg="white")
+        climb_label_r = tk.Label(text="Ropes:", fg="white")
+        climb_label_b.grid(row=7, column=1)
+        dropdown1.grid(row=7, column=2)
+        dropdown2.grid(row=7, column=3)
+        dropdown3.grid(row=7, column=4)
+        climb_label_r.grid(row=7, column=5)
+        dropdown4.grid(row=7, column=6)
+        dropdown5.grid(row=7, column=7)
+        calculate_score_button = tk.Button(root, text="Calculate Score", command=calculate_score_champs, fg="magenta")
+        calculate_score_button.grid(row=8, column=2, columnspan=3)
 
 
 def calculate_score_boulders():
@@ -116,11 +128,75 @@ def calculate_score_ropes():
     score_answer.grid(row=10, column=2, columnspan=3)
 
 
+def calculate_score_champs():
+    score = 0
+    climb1 = str(selected_boulder1.get())
+    climb2 = str(selected_boulder2.get())
+    climb3 = str(selected_boulder3.get())
+    climb4 = str(selected_boulder4.get())
+    climb5 = str(selected_boulder4.get())
+    climb_list = climb1, climb2, climb3, climb4, climb5
+    # print(climb_list)
+    for climb in climb_list:
+        # print(climb)
+        if climb == "N/A":
+            points = 0
+        elif climb == "V0":
+            points = 2
+        elif climb == "V1":
+            points = 3
+        elif climb == "V2":
+            points = 5
+        elif climb == "V3":
+            points = 7
+        elif climb == "V4":
+            points = 9
+        elif climb == "V5":
+            points = 11
+        elif climb == "V6":
+            points = 13
+        elif climb == "V7":
+            points = 15
+        elif climb == "V8":
+            points = 17
+        elif climb == "V9":
+            points = 19
+        elif climb == "V10":
+            points = 21
+        elif climb == "5.6":
+            points = 1
+        elif climb == "5.7":
+            points = 2
+        elif climb == "5.8":
+            points = 3
+        elif climb == "5.9":
+            points = 5
+        elif climb == "5.10-":
+            points = 7
+        elif climb == "5.10+":
+            points = 10
+        elif climb == "5.11-":
+            points = 11
+        elif climb == "5.11+":
+            points = 14
+        elif climb == "5.12-":
+            points = 15
+        elif climb == "5.12+":
+            points = 18
+        else:
+            points = 0
+        score = score+points
+    final_score_boulders = str(score)
+    score_statement = "Your current score is " + str(final_score_boulders)
+    score_answer = tk.Label(text=score_statement, fg="cyan")
+    score_answer.grid(row=10, column=2, columnspan=3)
+
+
 introduction = tk.Label(text="Welcome to the rock climbing competition points counter!", fg="magenta")
-climb_input_question = tk.Label(text="Is this a bouldering or top rope competition?", fg="magenta")
+climb_input_question = tk.Label(text="What type of competition is this?", fg="magenta")
 climb_input = tk.StringVar()
 climb_input.set("")
-dropdown0 = tk.OptionMenu(root, climb_input, "Bouldering", "Top Rope")
+dropdown0 = tk.OptionMenu(root, climb_input, "Bouldering", "Top Rope","Champs")
 # number_of_menus = str(climb_input.get())
 dropdown1 = tk.OptionMenu(root, selected_boulder1, "N/A", "V0", "V1", "V2", "V3", "V4", "V5", "V6", "V7", "V8", "V9", "V10")
 dropdown2 = tk.OptionMenu(root, selected_boulder2, "N/A", "V0", "V1", "V2", "V3", "V4", "V5", "V6", "V7", "V8", "V9", "V10")
